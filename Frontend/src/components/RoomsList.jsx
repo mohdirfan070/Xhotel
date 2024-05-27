1a45a561b4de43d457d806d3887663951a30df1e
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-05-28 01:27:14 +0530

    Vercel.Json

--- Frontend/src/components/RoomsList.jsx
+++ Frontend/src/components/RoomsList.jsx
@@ -53,7 +53,7 @@ function RoomsList() {
         }else{
           setResponse2(true);
           try{
-          await axios.post('https://xhotel-osu7.onrender.com/api/addcustomer', { inpData}).then((response)=>{
+          await axios.post('/api/addcustomer', { inpData}).then((response)=>{
             reset();
             getData();
             // setInpData({
