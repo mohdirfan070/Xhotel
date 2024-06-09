9fc3be70b31a196e519cb393999beef7896e68b3
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-09 16:40:44 +0530

    Updated DB URI

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <>
     <div className="hero min-h-screen  max-w-[80%] bg-base">
     <div className="hero-content flex-col lg:flex-row">
-      <img src="https://vistapointe.net/images/hotel-1.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+      <img src="https://vistapointe.net/images/hotel-1.jpg" className=" max-w-72  rounded-lg shadow-2xl" />
       <div>
         <h1 className="text-5xl font-bold">XHotel </h1>
         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
