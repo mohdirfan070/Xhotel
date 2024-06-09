7ef109aa8a3aeb85cebda9f863da21d07fc04c9b
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-09 22:16:00 +0530

    R

--- Backend/app.js
+++ Backend/app.js
@@ -1,40 +1,58 @@
 require('dotenv').config();
 const express  = require('express');
 const app = express();
-const mongoose = require('mongoose');
+
 const cors = require('cors');
+app.use(cors());
+
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB using Mongoose!');
+    // Define your schemas and models here
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+
+
 //  mongoose.connect('mongodb://localhost:27017/hotel').then(()=>{console.log("Successfully Connected To Database");});
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URL;
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URL;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(" successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(" successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 const User = require('./models/userSchema');
 const Room = require('./models/RoomSchema');
 
-app.use(cors());
+
 
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
